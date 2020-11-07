10a67ef23c7c4bf029650749fdadc8f4d2c3004d
seed <hellocarloscastillo@gmail.com>
2020-11-07 02:02:38 -0500

    asdasd

--- metodosnumericos/calculadora/urls.py
+++ metodosnumericos/calculadora/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import index, suma_resta
 urlpatterns = [
     path('', index),
+    path('suma_resta/', suma_resta, name = 'suma_resta'),
 ]
--- metodosnumericos/calculadora/views.py
+++ metodosnumericos/calculadora/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from calculadora.motores import SumaResta
 # Create your views here.
 
 
 def index(request):
 
     return render(request,'calculadora/index.html')
+
+def suma_resta(request):
+
+    return render(request, 'calculadora/Suma_Resta.html')
