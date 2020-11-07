3e517baa0e9e7b1d394a4ea7f581fa39ecc28834
seed <hellocarloscastillo@gmail.com>
2020-11-07 15:01:10 -0500

    asdasd

--- metodosnumericos/calculadora/urls.py
+++ metodosnumericos/calculadora/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index, suma_resta
+from .views import index, suma_resta, calcSumRest
 urlpatterns = [
     path('', index),
     path('suma_resta/', suma_resta, name = 'suma_resta'),
+    path('calcSumRest/', calcSumRest, name ='calcSumRest'),
 ]
--- metodosnumericos/calculadora/views.py
+++ metodosnumericos/calculadora/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from calculadora.motores import SumaResta
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+
 # Create your views here.
 
 
@@ -8,7 +13,14 @@ def index(request):
     return render(request,'calculadora/index.html')
 
 def suma_resta(request):
-
-    
-
     return render(request, 'calculadora/Suma_Resta.html')
+@csrf_exempt 
+def calcSumRest(request):
+    if request.is_ajax() and request.method == 'POST':
+        #print("respuesta : ", dict(request.POST.get('dats'))['mUno'])
+        mDos = json.loads(request.POST.get('dats'))['mUno']
+        #mDos = json.loads(request.POST.get('dats')[1])
+
+        return JsonResponse({'m':mDos, 'success':True})
+    
+    return JsonResponse({'success':False})
