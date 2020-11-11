4041003b6e195dd066d6de027fbbe5a1eff9f959
seed <hellocarloscastillo@gmail.com>
2020-11-11 18:11:26 -0500

    asdasd

--- metodosnumericos/calculadora/urls.py
+++ metodosnumericos/calculadora/urls.py
@@ -8,15 +8,15 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('/home', index,name="home"),
-    path('grafica', grafica, name="grafica"),
-    path('montecarlo', monteCarlo,name="montecarlo"),
+    path('home/', index,name="home"),
+    path('grafica/', grafica, name="grafica"),
+    path('montecarlo/', monteCarlo,name="montecarlo"),
 
-    path('primerCorte', primerCorte, name="primerCorte"),
-    path('segundoCorte', segundoCorte, name="segundoCorte"),
-    path('tercerCorte', tercerCorte, name="tercerCorte"),
+    path('primerCorte/', primerCorte, name="primerCorte"),
+    path('segundoCorte/', segundoCorte, name="segundoCorte"),
+    path('tercerCorte/', tercerCorte, name="tercerCorte"),
 
-    path('suma_resta', suma_resta, name = 'suma_resta'),
+    path('suma_resta/', suma_resta, name = 'suma_resta'),
 
     path('suma_resta/', suma_resta, name = 'suma_resta'),
 
